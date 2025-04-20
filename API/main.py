3dbd9b0e9218492bdf85616c6b10d4caef8f9a7b
Byron Lloyd Wakeman <72659565+byronlloydwakeman@users.noreply.github.com>
2025-04-20 21:26:19 +0100

    Update main.py

--- API/main.py
+++ API/main.py
@@ -52,7 +52,7 @@ async def test():
 async def supermarketSearchByLocation(locationModel: LocationModel):
     headers = {
         "accept": "application/json",
-        "Authorization": "bearer 0iJ6q77vDLoWeOod_0VRej8bNBLpEG-T79vNoDkhY0DM_PtcVEveTm6alZ0LtrHJ_476_JPhbPC1JtZbHn_kJLwe0NgReB-ouuB-sxE5bIC_6_-cvlmhSZgJOSSMZHYx"
+        "Authorization": "bearer yelp-api-key"
     }
 
     supermarkets = ["Waitrose", "Tesco", "Asda"]
